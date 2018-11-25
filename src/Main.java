810145afba050b1b270750d5d733e3a14a780bc5
normalek <filmacker@gmail.com>
2018-11-25 18:06:11 +0300

    comments for 1st task

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
 public class Main {
-
-	public static void method(String text, ArrayList<String> WordsArgs, boolean reg) {
+	//имена любых переменных должны всегда быть с маленькой буквы (кроме констант), речь про WordsArgs
+	public static void method(String text, ArrayList<String> WordsArgs, boolean reg) {//плохое имя для метода "method", надо переименовать
 		Date time = new Date();
 		long ttime = time.getTime();
-		System.out.println(ttime);
+		System.out.println(ttime); //для чего эта переменная? если это просто для отладки, то надо убрать
 
 		String words[] = text.split(" ");
 		int argIter = 0;
@@ -15,7 +15,7 @@ public class Main {
 			int wordsIter = 0;
 			int quant = 0;
 
-			while (wordsIter < words.length) {
+			while (wordsIter < words.length) { // подумай как переделать данный цикл на for без использования переменной wordsIter
 				if (reg) {
 					if (words[wordsIter].equals(WordsArgs.get(argIter)))
 						quant++;
@@ -26,26 +26,34 @@ public class Main {
 
 				wordsIter++;
 			}
-			System.out.println((argIter + 1) + "." + WordsArgs.get(argIter) + ":" + quant);
+			System.out.println((argIter + 1) + "." + WordsArgs.get(argIter) + ":" + quant); // желательно вывести пояснение что такой-то аргумент встречается столько то раз
 			argIter++;
 		}
 		
-		System.out.println(time.getTime());
+		System.out.println(time.getTime()); //если это для отладки, то надо убрать
 	}
 	
-	public static void method(String text, ArrayList<String> WordsArgs) {
+	public static void method(String text, ArrayList<String> WordsArgs) {// этот метод не используется?
 		method(text,WordsArgs,true);
 	}
 
+	/**
+	 * данная реализация не умеет искать слова, если они со знаками препинания
+	 * подумай как можно модифицировать, чтобы можно было распарсить вот такой текст:
+	 * Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	 * с параметром amet или elit
+	 */
 	public static void main(String args[]) {
 		Scanner scan = new Scanner(System.in);
-		ArrayList<String> WordsArgs = new ArrayList<String>();
+		ArrayList<String> WordsArgs = new ArrayList<>();// если используется Java 8 версии и выше, то не нужно указывать тип коллекции
 		System.out.println("Enable case-sensitive?(1 - yes, 0 - no)");
-		boolean reg = scan.nextBoolean();
+		boolean reg = scan.nextBoolean(); //почему boolean, если на предыдущем шаге ожидаем 1 или 0 ?
 
-		System.out.println("Input text");
+		scan.nextLine(); // без ручного перевода строки не работает, проверь еще раз
 
-		String text = scan.nextLine();
+		System.out.println("Input text:");
+
+		String text = scan.nextLine();// нужно добавить пояснение, что окончанием считывания параметров является знак пробела
 
 		do {
 			System.out.print("Input argument:");
@@ -57,8 +65,7 @@ public class Main {
 
 			WordsArgs.add(arg);
 
-		} while (!false);
-
+		} while (!false);// зачем !false если можно просто true ?
 		method(text, WordsArgs, reg);
 		scan.close();
 	}
